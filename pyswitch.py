5ab4d018537c16b259b56a7138713a268329573c
jdesgarennes <jdesgarennes@johns-mbp-4.pechanga.com>
2023-07-12 18:14:10 -0700

    added new batch mode

--- pyswitch.py
+++ pyswitch.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QFileDialog, QComboBox
+import csv
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QFileDialog, QMessageBox
 
 class SwitchConfig(QWidget):
     def __init__(self):
@@ -32,22 +34,17 @@ class SwitchConfig(QWidget):
         self.tacsecrete_entry = QLineEdit()
         layout.addWidget(self.tacsecrete_entry)
 
-        template_label = QLabel("Template:")
-        layout.addWidget(template_label)
-        self.template_combo = QComboBox()
-        self.template_combo.addItem("template1")
-        self.template_combo.addItem("template2")
-        layout.addWidget(self.template_combo)
+        button = QPushButton("Generate Single Config")
+        button.clicked.connect(self.generate_config)
+        layout.addWidget(button)
 
-        button_generate = QPushButton("Generate")
-        button_generate.clicked.connect(self.generate_config)
-        layout.addWidget(button_generate)
-
-        button_save = QPushButton("Save to File")
-        button_save.clicked.connect(self.save_to_file)
-        layout.addWidget(button_save)
+        batch_button = QPushButton("Batch Mode")
+        batch_button.clicked.connect(self.batch_mode)
+        layout.addWidget(batch_button)
 
         self.setLayout(layout)
+
+        self.show()  # Show the window
 
     def generate_config(self):
         hostname = self.hostname_entry.text()
@@ -55,41 +52,62 @@ class SwitchConfig(QWidget):
         passphrase = self.passphrase_entry.text()
         tacsecrete = self.tacsecrete_entry.text()
 
-        template = self.template_combo.currentText()
+        # Read the template file
+        with open('./templates/template.txt', 'r') as template_file:
+            template = template_file.read()
 
-        # Load the template file
-        with open("./templates/template.txt", "r") as file:
-            template_content = file.read()
+        # Replace the variables in the template with the provided values
+        config = template.replace('{{hostname}}', hostname)
+        config = config.replace('{{mgmt_ip}}', mgmt_ip)
+        config = config.replace('{{passphrase}}', passphrase)
+        config = config.replace('{{tacsecrete}}', tacsecrete)
 
-        # Replace the variables in the template
-        template_content = template_content.replace("{{hostname}}", hostname)
-        template_content = template_content.replace("{{mgmt_ip}}", mgmt_ip)
-        template_content = template_content.replace("{{passphrase}}", passphrase)
-        template_content = template_content.replace("{{tacsecrete}}", tacsecrete)
+        # Save the configuration to a file
+        self.save_to_file(config, hostname)
 
-        # Print or save the generated configuration
-        print("Generated switch configuration:")
-        print(template_content)
+    def batch_mode(self):
+        # Open a file dialog to select the CSV file
+        file_dialog = QFileDialog()
+        csv_path, _ = file_dialog.getOpenFileName(self, 'Select CSV File', '', 'CSV Files (*.csv)')
 
-        self.generated_config = template_content
+        if csv_path:
+            # Read the CSV file and generate configurations
+            with open(csv_path, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                    hostname = row['Hostname']
+                    mgmt_ip = row['Management IP']
+                    passphrase = row['Passphrase']
+                    tacsecrete = row['Tacsecrete']
 
-    def save_to_file(self):
-        if hasattr(self, 'generated_config'):
-            # Open a file dialog to select the save location
-            file_dialog = QFileDialog()
-            file_path, _ = file_dialog.getSaveFileName(self, 'Save File', '', 'Text Files (*.txt)')
+                    # Read the template file
+                    with open('./templates/template.txt', 'r') as template_file:
+                        template = template_file.read()
 
-            if file_path:
-                # Write the generated configuration to the selected file
-                with open(file_path, 'w') as file:
-                    file.write(self.generated_config)
+                    # Replace the variables in the template with the provided values
+                    config = template.replace('{{hostname}}', hostname)
+                    config = config.replace('{{mgmt_ip}}', mgmt_ip)
+                    config = config.replace('{{passphrase}}', passphrase)
+                    config = config.replace('{{tacsecrete}}', tacsecrete)
 
-                print(f'File saved to: {file_path}')
-        else:
-            print("No configuration generated yet.")
+                    # Save the configuration to a file
+                    self.save_to_file(config, hostname)
+
+            QMessageBox.information(self, "Batch Mode", "Batch configuration generation completed.")
+
+    def save_to_file(self, config, hostname):
+        output_dir = "output"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        file_path = os.path.join(output_dir, f"{hostname}.txt")
+
+        with open(file_path, 'w') as file:
+            file.write(config)
+
+        print(f'Configuration saved to: {file_path}')
 
 if __name__ == '__main__':
     app = QApplication([])
     window = SwitchConfig()
-    window.show()
     app.exec_()
